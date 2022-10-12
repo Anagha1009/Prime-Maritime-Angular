09e7df27f73a1b1f482fd3136488a4a0900a5931
Prachi1305 <prachig@PIPLMUMDT048>
2022-10-12 17:45:36 +0530

    CRO

--- Prime/src/app/modules/new-cro/new-cro.component.ts
+++ Prime/src/app/modules/new-cro/new-cro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CroService } from 'src/app/services/cro.service';
 
 @Component({
   selector: 'app-new-cro',
@@ -8,20 +10,62 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class NewCroComponent implements OnInit {
   croForm: FormGroup;
+  submitted: boolean = false;
 
-  constructor(private FormBuilder: FormBuilder) {}
+  constructor(
+    private FormBuilder: FormBuilder,
+    private _croService: CroService,
+    private _router: Router
+    ) {}
 
   ngOnInit(): void {
     this.croForm = this.FormBuilder.group({
+      BOOKING_ID:[1],
+      BOOKING_NO:['123'],
       STUFFING_TYPE: ['', Validators.required],
       EMPTY_CONT_PCKP: ['', Validators.required],
-      LADEN_ACPT_LOC: ['', Validators.required],
+      LADEN_ACPT_LOCATION: ['', Validators.required],
       RO_VALIDITY_DATE: ['', Validators.required],
       REMARKS: [''],
-      REQUIRED_QTY: ['', Validators.required],
-      GROSS_WEIGHT: ['', Validators.required],
-      PCKGS: [''],
-      NO_OF_PCKGS: [''],
+      REQ_QUANTITY: ['', Validators.required],
+      GROSS_WT: ['', Validators.required],
+      GROSS_WT_UNIT:['', Validators.required],
+      PACKAGES:['', Validators.required],
+      NO_OF_PACKAGES: [''],
+      STATUS: ['Drafted'],
+      AGENT_NAME: [''],
+      AGENT_CODE: [''],
+      CREATED_BY: ['']
     });
   }
+
+  get f() {
+    return this.croForm.controls;
+  }
+
+  SaveCRO() {
+
+    this.croForm
+    .get('AGENT_NAME')
+    ?.setValue(localStorage.getItem('username'));
+
+    this.croForm
+    .get('AGENT_CODE')
+    ?.setValue(localStorage.getItem('rolecode'));
+
+    this.croForm
+    .get('CREATED_BY')
+    ?.setValue(localStorage.getItem('username'));
+
+    console.log(JSON.stringify(this.croForm.value));
+    this._croService
+      .insertCRO(JSON.stringify(this.croForm.value))
+      .subscribe((res) => {
+        if (res.responseCode == 200) {
+          alert('Your CRO has been submitted successfully !');
+          this._router.navigateByUrl('home/agent-dashboard');
+        }
+      });
+  }
+
 }
