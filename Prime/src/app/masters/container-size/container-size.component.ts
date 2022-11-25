fa3a29cbb3de5931254e758263b40c41f5f5033b
Anagha1009 <anaghat@portall.in>
2022-11-25 14:44:48 +0530

    all masters

--- Prime/src/app/masters/container-size/container-size.component.ts
+++ Prime/src/app/masters/container-size/container-size.component.ts
@@ -1,117 +1,104 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CONTAINER } from 'src/app/models/container';
-import { SIZE } from 'src/app/models/size';
-import { SizeService } from 'src/app/services/size.service';
+import { MasterService } from 'src/app/services/master.service';
 
 @Component({
   selector: 'app-container-size',
   templateUrl: './container-size.component.html',
-  styleUrls: ['./container-size.component.scss']
+  styleUrls: ['./container-size.component.scss'],
 })
 export class ContainerSizeComponent implements OnInit {
   submitted: boolean = false;
   sizeForm: FormGroup;
   SizeList: any[] = [];
-  data: any;
   isUpdate: boolean = false;
-  size:SIZE;
- 
-  constructor(private _sizeService: SizeService, private _formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private _router: Router ) { }
+
+  constructor(
+    private _masterService: MasterService,
+    private _formBuilder: FormBuilder
+  ) {}
+
   ngOnInit(): void {
     this.sizeForm = this._formBuilder.group({
-      ID:[0],
-      CONT_SIZE: [''],
+      ID: [0],
+      KEY_NAME: [''],
+      CODE: [''],
+      CODE_DESC: [''],
       STATUS: [''],
-      CREATED_BY :[''],
-
+      CREATED_BY: [''],
     });
 
     this.GetContainerSizeList();
-}
+  }
 
-InsertContainerSize() {
-  this.sizeForm.get('CREATED_BY')?.setValue(localStorage.getItem('username'));
-  var status = this.sizeForm.get('STATUS')?.value;
-  this.sizeForm.get('STATUS')?.setValue(status == "true" ? true : false);
+  InsertContainerSize() {
+    this.sizeForm.get('CREATED_BY')?.setValue(localStorage.getItem('username'));
+    var status = this.sizeForm.get('STATUS')?.value;
+    this.sizeForm.get('STATUS')?.setValue(status == 'true' ? true : false);
+    this.sizeForm.get('KEY_NAME')?.setValue('CONTAINER_SIZE');
 
-  this._sizeService.postContainerSize(JSON.stringify(this.sizeForm.value)).subscribe((res: any) => {
-    if (res.responseCode == 200) {
-      alert('Your record has been submitted successfully !');
-      this.GetContainerSizeList()
-      this.ClearForm()
-    }
-  });
-   
+    this._masterService
+      .InsertMaster(JSON.stringify(this.sizeForm.value))
+      .subscribe((res: any) => {
+        if (res.responseCode == 200) {
+          alert('Your record has been submitted successfully !');
+          this.GetContainerSizeList();
+          this.ClearForm();
+        }
+      });
   }
 
   GetContainerSizeList() {
-    var sizeModel = new SIZE();
-    sizeModel.CREATED_BY = localStorage.getItem('usercode');
+    this._masterService
+      .GetMasterList('CONTAINER_SIZE')
+      .subscribe((res: any) => {
+        if (res.ResponseCode == 200) {
+          this.SizeList = res.Data;
+        }
+      });
+  }
 
-    this._sizeService.GetcontainerSizeList(sizeModel).subscribe((res: any) => {
-      if (res.ResponseCode == 200) {
-        this.SizeList = res.Data;
-      }
-    });
-   }
-
-  GetContainerSizeDetails(containersizeId: number) {
-    var containerSizeModel = new SIZE();
-    containerSizeModel.CREATED_BY = localStorage.getItem('usercode');
-    containerSizeModel.ID = containersizeId;
-    this._sizeService.GetContainerSizeDetails(containerSizeModel).subscribe((res: any) => {
+  GetContainerSizeDetails(ID: number) {
+    this._masterService.GetMasterDetails(ID).subscribe((res: any) => {
       if (res.ResponseCode == 200) {
         console.log(res.Data);
-        this.sizeForm.patchValue(res.Data)
+        this.sizeForm.patchValue(res.Data);
         this.isUpdate = true;
       }
     });
   }
 
-  UpdateContainerSize(){
+  UpdateContainerSize() {
     this.sizeForm.get('CREATED_BY')?.setValue(localStorage.getItem('username'));
 
     var status = this.sizeForm.get('STATUS')?.value;
-    this.sizeForm.get('STATUS')?.setValue(status == "true" ? true : false);
+    this.sizeForm.get('STATUS')?.setValue(status == 'true' ? true : false);
 
-    this._sizeService
-      .UpdateContainerSize(JSON.stringify(this.sizeForm.value))
+    this._masterService
+      .UpdateMaster(JSON.stringify(this.sizeForm.value))
       .subscribe((res: any) => {
         if (res.responseCode == 200) {
           alert('Your party master has been Updated successfully !');
-          this.GetContainerSizeList()
-          this.sizeForm.setValue(this.size);
-          this.ClearForm()
+          this.GetContainerSizeList();
+          this.ClearForm();
           this.isUpdate = false;
-        }    
+        }
       });
   }
 
   ClearForm() {
-    this.sizeForm.reset()
-    this.sizeForm.get('CONT_SIZE')?.setValue('');
-    this.sizeForm.get('STATUS')?.setValue(''); 
+    this.sizeForm.reset();
+    this.sizeForm.get('STATUS')?.setValue('');
   }
 
-  DeleteContainerSize(sizeId: number) {
-
-    if(confirm('Are you sure want to delete this record ?')){
-      var sizeModel = new SIZE();
-      sizeModel.CREATED_BY = localStorage.getItem('usercode');
-      sizeModel.ID = sizeId;
-
-    this._sizeService.DeleteContainerSize(sizeModel).subscribe((res: any) => {
-      if (res.ResponseCode == 200) {
-        alert('Your record has been deleted successfully !');
-        this.GetContainerSizeList();
-      }
-    });
+  DeleteContainerSize(ID: number) {
+    if (confirm('Are you sure want to delete this record ?')) {
+      this._masterService.DeleteMaster(ID).subscribe((res: any) => {
+        if (res.ResponseCode == 200) {
+          alert('Your record has been deleted successfully !');
+          this.GetContainerSizeList();
+        }
+      });
     }
   }
-
-
 }
