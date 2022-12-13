db01cf9297bbb182e40cb6caa12ffedcf4a05fae
Anagha1009 <anaghat@portall.in>
2022-12-13 10:12:44 +0530

    load list

--- Prime/src/app/shared/header/header.component.ts
+++ Prime/src/app/shared/header/header.component.ts
@@ -32,11 +32,8 @@ export class HeaderComponent implements OnInit {
       this.menuList.push(
         { menuName: 'Rate Requests', menuLink: 'home/quotation-list' },
         { menuName: 'Bookings', menuLink: 'javascript:void(0)' },
-        { menuName: 'CRO', menuLink: 'home/cro-list' },
-        { menuName: 'DO', menuLink: 'home/do-list' },
-        { menuName: 'BL', menuLink: 'home/new-bl' },
-        { menuName: 'Container Movement', menuLink: 'home/new-cm' },
-        { menuName: 'Container Tracking', menuLink: 'home/tracking' },
+        { menuName: 'Operations', menuLink: 'javascript:void(0)' },
+        { menuName: 'Container Movement', menuLink: 'javascript:void(0)' },
         { menuName: 'Finance', menuLink: 'javascript:void(0)' }
       );
     }
