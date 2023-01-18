18ca1f2246a30d176a80c2c8d4b7d479c28286a1
archanac <archanac@PIPLMUMDT073>
2023-01-18 13:03:38 +0530

    Recent

--- Prime/src/app/@core/translate/srr/hi.ts
+++ Prime/src/app/@core/translate/srr/hi.ts
@@ -7,7 +7,19 @@ export const locale = {
       GeneralInfo:'सामान्य जानकारी',
       CommodityDetails:'कमोडिटी विवरण',                        
       ContainerRateDetails:'कंटेनर और दर विवरण',
-      
+      CommodityFiles:'कमोडिटी फ़ाइलें',
+      SRRNo:'एसआरआर संख्या',
+      PlaceofReceipt:'रसीद संख्या का स्थान',
+      PlaceofDelivery:'डिलिवरी का स्थान',
+      FinalDestination:'अंतिम गंतव्य',
+      PortofLoading:'माल लदान का बंदरगाह',
+      PortofDischarge:'जहाज से माल उतारने का स्थान',
+      Transhipment1:'ट्रांसशिपमेंट 1',
+      Transhipment2:'ट्रांसशिपमेंट 2',
+      ServiceName:'सेवा का नाम',
+      EffectFrom:'से प्रभाव',
+      EffectTo:'करने के लिए प्रभाव',
+      CustomerName:'ग्राहक का नाम',
 
 
       requestNo: 'अनुरोध संख्या',
