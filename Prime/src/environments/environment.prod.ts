43694ceb3b00efc9d62b626ad49b82c4d8ff7da2
mariak <mariak@PIPLMUMDT039>
2023-02-14 10:36:17 +0530

    latest

--- Prime/src/environments/environment.prod.ts
+++ Prime/src/environments/environment.prod.ts
@@ -1,13 +1,7 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
    BASE_URL: 'https://localhost:44316/api/',
   // BASE_URL: 'http://10.1.1.121/PRIME/api/',
   //BASE_URL: 'https://etariff.jmbaxi.com/PRIME/api/',
-=======
-  BASE_URL: 'https://localhost:44316/api/',
-  // BASE_URL: 'http://10.1.1.121/PRIME/api/',
-  // BASE_URL: 'https://etariff.jmbaxi.com/PRIME/api/',
->>>>>>> 9c3a633abcdc9f06e6c873cf60aa541f792a3788
   BASE_URL2: 'https://localhost:44316/',
 };
