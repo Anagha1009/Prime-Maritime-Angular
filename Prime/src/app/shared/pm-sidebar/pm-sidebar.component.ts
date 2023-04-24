8387725a60008f5eecb491d930cfcfcb7f48424b
Anagha1009 <anaghat@portall.in>
2023-04-24 13:00:24 +0530

    chnagesss

--- Prime/src/app/shared/pm-sidebar/pm-sidebar.component.ts
+++ Prime/src/app/shared/pm-sidebar/pm-sidebar.component.ts
@@ -10,25 +10,12 @@ import { LoginService } from 'src/app/services/login.service';
 export class PmSidebarComponent implements OnInit {
   ismenu: boolean = false;
   submenu: string = '';
+  menu: string = 'dashboard';
   @Input() username: string;
 
   constructor(private _router: Router, private _loginService: LoginService) {}
 
-  ngOnInit(): void {
-    // this.loadJsFile([
-    //   'assets/pm-assets/vendor/js/helpers.js',
-    //   'assets/pm-assets/js/config.js',
-    //   'assets/pm-assets/vendor/libs/jquery/jquery.js',
-    //   'assets/pm-assets/vendor/libs/popper/popper.js',
-    //   'assets/pm-assets/vendor/js/bootstrap.js',
-    //   'assets/pm-assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js',
-    //   'assets/pm-assets/vendor/js/menu.js',
-    //   //'./../../../assets/pm-assets/vendor/libs/apex-charts/apexcharts.js',
-    //   'assets/pm-assets/js/main.js',
-    //   // './../../../assets/pm-assets/js/dashboards-analytics.js',
-    //   'https://buttons.github.io/buttons.js',
-    // ]);
-  }
+  ngOnInit(): void {}
 
   public loadJsFile(url: any[]) {
     url.forEach((el) => {
@@ -43,6 +30,11 @@ export class PmSidebarComponent implements OnInit {
     this.ismenu = true;
     this.submenu = value;
     this._router.navigateByUrl('/master/' + value);
+  }
+
+  redirectToMenu(value: string) {
+    this.menu = value;
+    this._router.navigateByUrl('/pm/' + value);
   }
 
   logout() {
